68af3da0c17d3715067ee1a85eb942af11c62108
Sahil Kirad <kiradsahil882@gmail.com>
2025-07-25 18:32:50 +0530

    refactor: update signup route to handle async user creation

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,14 +1,23 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const {User}=require('../db');
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement user signup logic
+    const username=req.body.username;
+    const password=req.body.password;
+    await User.create({
+        username:username,
+        password:password
+    });
+    res.json({message: 'User created successfully'})
+
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
